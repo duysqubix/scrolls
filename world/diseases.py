5092cf6ab79c840cb5bd58e324164dbc343ea9b6
j3b4 <juggins@gmail.com>
2020-12-16 23:51:01 -0400

    brain rot

--- world/diseases.py
+++ world/diseases.py
@@ -4,3 +4,54 @@ class Disease:
 
 class Ataxia(Disease):
     """
+    Ataxia is a commona and relatively mild disease found across
+    the breadth of Tamriel. Its symptoms include general pain and
+    soreness, muscle stiffness, and anemia. Victims of Ataxia are
+    often pale, groggy, and irritable. Some particularly devious
+    bandits and trap-makers are known to spring vials or lace nails
+    with the bodily fluids of dead animals that carry the disease.
+
+    Symptoms
+    --------
+    Loss of Agility and Strength affecting Subterfuge and Block 
+    abilities.
+
+    Carriers
+    --------
+    Cave Rats; Bears; Alit; Giant Spiders; Slaughterfish; Zombies;
+    Traps
+
+    Prognosis
+    ---------
+    Symptoms are mild for one week. If not cured they can increase
+    in severity any time after seven days. 
+    """
+    pass
+
+
+class BrainRot(Disease):
+    """
+    Brain Rot is a progressively worsening, mind affecting disease.
+    It dampens the intelligence, intuition, and mental acuity of its
+    victims. Eventually, Brain Rot destroys any semblance of sanity
+    and of the self in its victims, leaving them to shuffle in a broken
+    stupor until they are cured or die.
+
+    Symptoms
+    --------
+    Loss of Magicka, Intelligence, and Personality, in that order as 
+    the disease progresses.
+
+    Carriers
+    --------
+    Cave Rats; Zombies; Hagravens; Traps
+
+    Prognosis
+    ---------
+    Loss of Magicka starts immediately after infection. After one week
+    there is a chance that Intelligence will be affected as well. If 
+    that occurs then after a further week it is possible to suffer the
+    loss of Personality as well, unless cured.
+    """
+    pass
+
