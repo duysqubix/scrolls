f4818a92166a29c4e7892f3aee3d5ae726ab7fc2
j3b4 <juggins@gmail.com>
2020-12-17 21:30:33 -0400

    more disease yum

--- resources/help_entries/diseases.py
+++ resources/help_entries/diseases.py
@@ -11,7 +11,7 @@ from resources.help_entries.birthsigns import HELP_CATEGORY
 from evennia import create_help_entry
 from evennia.help.models import HelpEntry
 from evennia.utils import dedent
-from world.diseases import ALL_DISEASES
+from world.diseases import ALL_DISEASES  # TODO: actually create that there
 from resources.help_entries.help_entry_wrapper import HelpEntryWrapper
 
 HELP_CATEGORY = 'disease'
@@ -25,16 +25,38 @@ class RaceSignHelpEntry(HelpEntryWrapper):
 
 diseases = ", ".join([x.name.capitalize() for x in ALL_DISEASES])
 
+# TODO: cut in a main entry text
 main_entrytxt = \
 f"""
+Diseases are contracted from contact with diseased people or
+animals, or other creatures that are otherwise filthy, such as
+skeevers or mudcrabs. Sometimes diseases can be caught as
+the result of traps, poisons, or environmental effects, such as
+Corpus.
+
+Common diseases are caught most often from traps, poisons, 
+or from fighting diseased animals or people. Common diseases
+often start out relatively minor but some progress severely.
+
+Common diseases bear mild social stigma, which can escalate
+to disgust and suspicion if it is allowed to develop into later
+stages. Given the ease of treating common diseases, only the 
+truly destitute and unfaithul are afflicted with what is seen as
+their just due.
+
+All common diseases can be cured by using any level Cure 
+Disease spell or potion. Any common disease can also be cured
+by sincere repentance and piousness at a shrine to one of the
+Eight and One. Infected animals typically ignore the effects of
+the common diseases they carry.
 
 {diseases}
 
-more help is available on the them by |ghelp <disease>|n
+More help is available on the them by |ghelp <disease>|n
 """
 DiseaseHelpEntry.create_main(HELP_CATEGORY, main_entrytxt)
 
-for sign in ALL_DISEASES:
+for disease in ALL_DISEASES:
     tmp = DiseaseHelpEntry(disease)
 
     entry = HelpEntry.objects.find_topicmatch(tmp.key)
--- world/diseases.py
+++ world/diseases.py
@@ -1,4 +1,5 @@
 class Disease:
+    __obj_name__ = ""
     pass
 
 
@@ -26,6 +27,7 @@ class Ataxia(Disease):
     Symptoms are mild for one week. If not cured they can increase
     in severity any time after seven days. 
     """
+    __obj_name__ = "ataxia"
     pass
 
 
@@ -53,5 +55,254 @@ class BrainRot(Disease):
     that occurs then after a further week it is possible to suffer the
     loss of Personality as well, unless cured.
     """
+    __obj_name__ = "brain rot"
     pass
 
+
+class BoneBreak(Disease):
+    """
+    Bone Break Fever is particularly cruel disease. Carried primarily
+    by rats and bears, the disease aggressively attacks the victim's
+    bones until they are vulnerable and pront to breakage. Untreated
+    Bone Break Fever often results in crippled, or outtright severed
+    limbs because of the fragility it causes in its victims.
+
+    Symptoms
+    --------
+    Loss of strength and endurance in the early stages, progressing to
+    weakness to Shock at the later stage.
+
+    Carriers
+    --------
+    Cave Rats; Bears; Wolves
+
+    Prognosis
+    ---------
+    After a three day incubation period, early stage symptoms appear. 
+    After a week late stage may develop.
+    """
+    __obj_name__ = "bone break fever"
+    pass
+
+
+class BloodLung(Disease):
+    """
+    A relatively minor, non life-threatening illness, Blood Lung
+    causes bleeding cysts in the victim's lungs. They are prone to
+    rupture at inconvenient times or times of physical exertion.
+    While Blood Lung is not severem and will not typically worsen, it
+    does open the door for other diseases to take root, and can also
+    server as a vector for others via the coughed up blood.
+
+    Symptoms
+    --------
+    Loss of endurance, increase in fatigue, and 
+    << every SP used after inflicts 1d4 of damage ignoring armour>>
+
+    Carriers
+    --------
+    Cave Rats; Nix-Hounds
+
+    Prognosis
+    ---------
+    Symptoms take effect after a three day incubation stage and persist 
+    unil cured.
+    """
+    __obj_name__ = "blood lung"
+    pass
+
+
+class  BlackHeart(Disease):
+    """
+    Black Heart Blight, despite being a member of the Blight family
+    of diseases created by Dagoth Ur, is both a blight and a 
+    common disease and is less severe than its cousins. After the
+    ending of the Blight in 3E 427, Black Heart Blight survived after
+    making the jump to undead. It is carried by zombies exclusively
+    after the destruction of Dagoth Ur, but prior to his death it was
+    also carried by the various blighted beasts or Corprus monsters
+    of Morrowind. It is an acute disease that saps the victim's vitality
+    and endurance.
+
+    Symptoms
+    --------
+    Initial loss of endurance; followed by loss of carrying capacity in
+    late stage.
+
+    Carriers
+    --------
+    Zombies
+
+    Prognosis
+    ---------
+    Symptoms occur imediately after infection and worsen (enter late stage)
+    after three days.
+    """
+    __obj_name__ = "black heart blight"
+    pass
+
+
+class  Chills(Disease):
+    """
+    The Chills are a punishing infection caused by contact with the
+    undead. It affects the victim's mind and motor skills equally,
+    resulting in confused, inarticulate stupors. Its other symptoms
+    include an overwhelming sensation of cold that is not affected
+    by the warmth of any fire, nor the light of the sun. It has lead
+    to the death of many an unfortuneate adventurer, lost, alone, 
+    and cold in the crypts that dot Tamriel.
+
+    Symptoms
+    --------
+    Loss of intelligence, willpower, and agility and general impairment
+    to all mental and motor skills.
+
+    Carriers
+    --------
+    Zombies; Bonelord; Bonewalker
+
+    Prognosis
+    ---------
+    The disease incubated for only one hour, then suddenly overcomes 
+    the victim and all symptoms occur at once.
+    """
+    __obj_name__ = "chills"
+    pass
+
+
+class  Collywobbles(Disease):
+    """
+    Collywobbles' main symptoms are uncontrollable shaking and 
+    chronic ache. The constant minor shaking and spasming can
+    become debilitating, and the strain of the shakes causes muscle
+    soreness.
+
+    Symptoms
+    --------
+    Inital impairment to agility and endurance; followed by worsenning
+    symptoms and <<reduce max ap>>.
+
+    Carriers
+    --------
+    Zombies; Shalks
+
+    Prognosis
+    ---------
+    Secondary symptoms may start occuring anytime after the first week.
+    """
+    __obj_name__ = "collywobbles"
+    pass
+
+
+class  Dampworm(Disease):
+    """
+    Dampworm is a parasite that infests the victim's musculature,
+    slithering in between the tissue. It can be felt occasionlly
+    moving underneath the skin, which is a revolting and disturbing
+    experience to say the least. Its symptoms include minor twitches
+    and lapses in gross motor skill, as well as uncontrollable sweating.
+
+    Symptoms
+    --------
+    Reduction in the host's speed.
+
+    Carriers
+    --------
+    Nix-Hounds; Deer; Horses; Falmer
+
+    Prognosis
+    ---------
+    Dampworm symptoms remain stable over time.
+    """
+    __obj_name__ = "dampworm"
+    pass
+
+
+class  Droops(Disease):
+    """
+    The Droops are a notorious and serious common disease that
+    result in weakened and exhausted muscles. They result in 
+    excessive fatigue after virtually any physical exertion.
+
+    Symptoms
+    --------
+    Loss of strength, loss of stamina.
+
+    Carriers
+    --------
+    Zombies; Sheep; Kwama
+
+    Prognosis
+    ---------
+    Symptoms remain stable over time. 
+    """
+    __obj_name__ = "the droops"
+    pass
+
+
+class  Frostlimb(Disease):
+    """
+    A relatively obscure disease, Frostlimb causes intense sensations
+    of coldness within the victim's arms and legs, and especially their
+    fingers. As a result, fine motor skills are punished greatly, and
+    the effects of Frost damage is greatly magnified.
+
+    Symptoms
+    --------
+    Weakness to Frost; impairment of all fine motor skills.
+
+    Carriers
+    --------
+    Trolls, Mammoths, Falmer
+
+    Prognosis
+    ---------
+    Symptoms remain stable over time.
+    """
+    __obj_name__ = "frostlimb"
+    pass
+
+
+class  Disease(Disease):
+    """
+    """
+    __obj_name__ = "disease name"
+    pass
+
+
+class  Disease(Disease):
+    """
+    """
+    __obj_name__ = "disease name"
+    pass
+
+
+class  Disease(Disease):
+    """
+    """
+    __obj_name__ = "disease name"
+    pass
+
+
+class  Disease(Disease):
+    """
+    """
+    __obj_name__ = "disease name"
+    pass
+
+
+class  Disease(Disease):
+    """
+    """
+    __obj_name__ = "disease name"
+    pass
+
+
+class  Disease(Disease):
+    """
+    """
+    __obj_name__ = "disease name"
+    pass
+
+
+ALL_DISEASES = (Ataxia(), BrainRot(), BoneBreak(),  )
