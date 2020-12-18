f8531f0ee4e3bce2d23d23ae04d670e7766f1994
j3b4 <juggins@gmail.com>
2020-12-18 17:34:56 -0400

    upto tunnel cough

--- world/diseases.py
+++ world/diseases.py
@@ -263,46 +263,233 @@ class  Frostlimb(Disease):
     pass
 
 
-class  Disease(Disease):
+class  Greenspore(Disease):
     """
+    Carried primarily by slaughterfish, Greenspore is a mind affecting
+    fungus that causes irritability and mild dementia. The spores
+    take root in the victim's mind, but are easily treated in the early
+    stages. Mature Greenspore can cause permanent brain damage.
+
+    Symptoms
+    --------
+    At onset loss of peronality which progressively worsens in late stages.
+
+    Carriers
+    --------
+    Slaugherfish; Zombies
+
+    Prognosis
+    ---------
+    After a week personality damage may begin to deteriorate very rapidly.
     """
-    __obj_name__ = "disease name"
+    __obj_name__ = "greenspore"
     pass
 
 
-class  Disease(Disease):
+class  Helljoint(Disease):
     """
+    Helljoint is a mild, inflammatory disease that causes swelling
+    of the joints and mild irritating pain. It is extremely common 
+    in Northern climes, and is easily contracted and cured.
+
+    Symptoms
+    --------
+    Imediately damages the victim's agility and inhibits the ability to run
+    and sprint.
+
+    Carriers
+    --------
+    Zombies; Cliff Racers; Wolves
+
+    Prognosis
+    ---------
+    Symptoms remain stable over time.
     """
-    __obj_name__ = "disease name"
+    __obj_name__ = "helljoint"
     pass
 
 
-class  Disease(Disease):
+class  Rattles(Disease):
     """
+    The Rattles is a mild disease of subtle nervous ticks. The Rattles
+    cause light finger ticks and forgetfulness. Victims of the Rattles
+    are reported as appearing restless to the point of irritation,
+    though the sufferers of the disease aren't aware of their ticks
+    unless paying deliberate attention.
+
+    Symptoms
+    --------
+    Losse of agility and willpower.
+
+    Carriers
+    --------
+    Cave Rats; Nix-Hounds; Chaurus; Zombies; Skeletons
+
+    Prognosis
+    ---------
+    Symptoms remain stable over time.
     """
-    __obj_name__ = "disease name"
+    __obj_name__ = "the rattles"
     pass
 
 
-class  Disease(Disease):
+class  RedFever(Disease):
     """
+    A notorious and common illness, Red Fever often hits during
+    late autumn, or through contact with wild wolves or dogs. The
+    fever is short lived but intense, causing intense sweating, fever,
+    vomiting, and often other unpleasant bodily functions. Red
+    Fever is known to put even the mightiest warrior into fits of
+    anguish for its brief, but hellish duration.
+
+    Symptoms
+    --------
+    Chance to become utterly debilitated for a 24 hour period. 
+
+    Carriers
+    --------
+    Wolves, Dogs, Cave Rats
+
+    Prognosis
+    ---------
+    Incubates for one day, then accute phase lasts for three days after
+    which the disease "cures" itself.
     """
-    __obj_name__ = "disease name"
+    __obj_name__ = "red fever"
     pass
 
 
-class  Disease(Disease):
+class  Rockjoint(Disease):
     """
+    Rockjoint is a pervasive and ubiquitous disease across Tamriel.
+    Every adventurer, noble lord, or peasant farmer has either had
+    Rockjoint, or has personally known someone with Rockjoing.
+    Rockjoint swells the knees and elbows with sensitive and painful
+    fluids that bloat and then stiffen, and can lead to total
+    immobility if left unchecked.
+
+    Symptoms
+    --------
+    In the early stages Rockjoint impairs strength and agility and
+    as it progresses these impairments scale up dramatically. In 
+    its final stage, Rockjoint completely immobilizes its victim.
+
+    Carriers
+    --------
+    Guar; Alit; Wolves; Cave Rats; Bears; Horkers; Foxes; Traps;
+    Zombies
+
+    Prognosis
+    ---------
+    After one week it becomes possible to advance to more serious
+    stages which will last a mimimum of another week before it 
+    becomes possible to reach the final stage.
     """
-    __obj_name__ = "disease name"
+    __obj_name__ = "rockjoint"
     pass
 
 
-class  Disease(Disease):
+class  RustChancre(Disease):
     """
+    Rust Chancre is a mild disease that affects the victim's skin.
+    Blistering, itchy rashes break out in random patches across
+    the victim's body, often including their face and neck. The
+    crackled rashes are quite off-putting to others, and irritating
+    to the sufferer, but ultimately it is not a threatening disease.
+    Rust Chancre often leaves permanent scarring after it is cured.
+
+    Symptoms
+    --------
+    Slight reduction in willpower and disfigurement. 
+
+    Carriers
+    --------
+    Cave Rats; Zombies; Traps
+
+    Prognosis
+    ---------
+    Rust Chancre symptoms remain stable, though breakouts may come and
+    go. After being cured, the disease leaves permanent ugly scars.
     """
-    __obj_name__ = "disease name"
+    __obj_name__ = "rust chancre"
     pass
 
+
+class Shakes(Disease):
+    """
+    The Shakes are a mild diseases contracted from rats. It is 
+    comparable to a much less severe form of the Rattles, that affect the
+    entire body rather than just the fingers and face.
+
+    Symptoms
+    --------
+    Impaired agility.
+
+    Carriers
+    --------
+    Cave Rats
+
+    Prognosis
+    ---------
+    Symptoms remain stable until cured.
+    """
+    __obj_name___ =  "shakes"
+    pass
+
+
+class SwampFever(Disease):
+    """
+    Swamp Fever is a mild disease carried typically by mudcrabs and
+    other aquatic and amphibious vermin. Swamp Fever causes a 
+    high body temperature and cold sweats, and is highly contagious,
+    but largely non-threatening. The Bitter Coast of Morrowind is
+    notorious for extremely high rates of Swamp Fever.
+
+    Symptoms
+    --------
+    Moderate impairment of strength and endurance; high liklihood
+    of infecting others in close contact <<Diseased -30>>
+
+    Carriers
+    --------
+    Mudcrabs; Cave Rats; Slaugherfish; Crocodiles; Dreugh;
+    Giant Snakes
+
+    Prognosis
+    ---------
+    After one week, the Swamp Fever subsides.
+    """
+    __obj_name___ =  "swamp fever"
+    pass
+
+
+class TunnelCough(Disease):
+    """
+    Tunnel Cough is a largely benign disease that affects mostly
+    miners and other cave divers and dungeon dwellers. It is characterized
+    by dry, hiccup-like coughs. The coughs are not painful or 
+    debilitating, but are certainly an inconvenience.
+
+    Symptoms
+    --------
+    The chronic coughing impairs all stealthy maneuvers.
+
+    Carriers
+    --------
+    Caused by exposure to ore dust.
+
+    Prognosis
+    ---------
+    Symptoms remain stable until cured.
+    """
+    __obj_name___ =  "tunnel cough"
+    pass
+
+
+class Shakes(Disease):
+    """
+    """
+    __obj_name___ =  "shakes"
+    pass
 
 ALL_DISEASES = (Ataxia(), BrainRot(), BoneBreak(),  )
